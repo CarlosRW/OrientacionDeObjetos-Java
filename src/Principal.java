8be1806b9cf70d0e613e95f12cb474c3002ab78e
Carlos / <153391284+CarlosRW@users.noreply.github.com>
2025-05-05 19:57:48 -0600

    Modificadores de acceso
    Getters, Setters, Encapsulamiento.

--- src/Principal.java
+++ src/Principal.java
@@ -1,35 +1,48 @@
+import carlosramirez.screenmach.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         // Orientación de objetos
-        // Llama a la clase Pelicula
+        // Llama a la clase carlosramirez.screenmach.modelos.Pelicula
         Pelicula pelicula = new Pelicula();
 
         // Da información a las variables
-        pelicula.nombre = "Iron Man";
-        pelicula.fechaDeLanzamiento = 2008;
-        pelicula.duracionEnMinutos = 120;
+        pelicula.setNombre("The Amazing Spider-Man");
+        pelicula.setFechaDeLanzamiento(2012);
+        pelicula.setDuracionEnMinutos(136);
 
         // Imprimir datos de la película
         pelicula.muestraDatosDePelicula();
         // Evalua la película
-        pelicula.evaluarPelicula(8.5);
+        pelicula.evaluarPelicula(9.5);
         pelicula.evaluarPelicula(10);
+        pelicula.evaluarPelicula(9.8);
+        pelicula.setIncluidoEnElPlan(true);
 
+        // Muestra total de evaluaciones
+        System.out.println("Total de evaluaciones: " + pelicula.getTotalDeLasEvaluaciones());
         // Media de la película
         System.out.println("Media de evaluaciones de la película: " + pelicula.calculaMediaEvaluaciones());
 
-        // Da información a las variables
-        Pelicula pelicula2 = new Pelicula();
-        pelicula2.nombre = "The Amazing Spider-Man";
-        pelicula2.fechaDeLanzamiento = 2012;
-        pelicula2.duracionEnMinutos = 136;
 
-        // Imprimir datos de la película
-        pelicula2.muestraDatosDePelicula();
-        pelicula2.evaluarPelicula(10);
-        pelicula2.evaluarPelicula(10);
 
-        // Media de la película
-        System.out.println("Media de evaluaciones de la película: " + pelicula.calculaMediaEvaluaciones());
+
+
+
+
+//        // Da información a las variables
+//        carlosramirez.screenmach.modelos.Pelicula pelicula2 = new carlosramirez.screenmach.modelos.Pelicula();
+//        pelicula2.nombre = "The Amazing Spider-Man";
+//        pelicula2.fechaDeLanzamiento = 2012;
+//        pelicula2.duracionEnMinutos = 136;
+
+//        // Imprimir datos de la película
+//        pelicula2.muestraDatosDePelicula();
+//        // Evaluaciones
+//        pelicula2.evaluarPelicula(10);
+//        pelicula2.evaluarPelicula(10);
+//
+//        // Media de la película
+//        System.out.println("Media de evaluaciones de la película: " + pelicula.calculaMediaEvaluaciones());
     }
 }
---
+++ src/carlosramirez/screenmach/modelos/Pelicula.java
@@ -0,0 +1,63 @@
+package carlosramirez.screenmach.modelos;
+
+public class Pelicula {
+    // Variables
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan = true;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones;
+
+    // Método para mostrar los datos de las películas
+    public void muestraDatosDePelicula() {
+        System.out.println("Nombre de la película: " + nombre);
+        System.out.println("Fecha de lanzamiento: " + fechaDeLanzamiento);
+        System.out.println("Duracion: " + duracionEnMinutos);
+    }
+
+    // Método para evaluar las películas
+    public void evaluarPelicula(double nota) {
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    // Método para calcular la media
+    public double calculaMediaEvaluaciones() {
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+
+    // Getters & Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public int getTotalDeLasEvaluaciones() {
+        return totalDeLasEvaluaciones;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+}
